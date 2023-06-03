1c6d1cb81e3652146e40a58bd2a7f0d10a065e08
Sajarappuz <sajarmohammed71@gmail.com>
2023-06-03 22:00:39 +0530

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,14 @@
+const initApp = () =>{
+    const hamBurgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = ()=>{
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamBurgerBtn.classList.toggle('toggle-btn')
+    }
+    hamBurgerBtn.addEventListener('click',toggleMenu)
+    mobileMenu.addEventListener('click',toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded',initApp)
